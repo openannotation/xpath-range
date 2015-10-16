ef0e16e5e4a463f94281d4b30b0a43865906ff13
Randall Leeds <randall@bleeds.info>
2015-10-15 20:44:54 -0700

    Always add wgxpath in tests

--- test/adapter.js
+++ test/adapter.js
@@ -1,5 +1,3 @@
 window.assert = require('assertive-chai').assert;
-if (require('is-ie8')) {
-  require('rangy').shim();
-  require('wgxpath').install();
-}
+if (require('is-ie8')) require('rangy').shim();
+require('wgxpath').install();
